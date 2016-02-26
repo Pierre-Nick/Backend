66bfc67b16fe9801cce6210b08efffa75a9ffbd6
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-02-26 14:32:23 -0500

    Testing Completed
    Script has completed basic testing!

--- packages/Items/addItem.py
+++ packages/Items/addItem.py
@@ -9,9 +9,13 @@
 #####          new item for a user to the inventory           #####
 ###################################################################
 
-from datetime import datetime
+from datetime import *
 from urllib2 import urlopen
 import json
+import MySQLdb
+
+debug_on = True
+log_level = 3
 
 def log(message, lev):
     # Log messages
@@ -40,7 +44,7 @@ def __check_vaild_date(key):
     # Checks if key vaild date has passed
     # return bool
     d = str(__get_session_key_expire_data(key))
-    d1 = datetime.datetime.strptime(d, "%d/%m/%Y").date()
+    d1 = datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
     d2 = datetime.now()
     return d1 > d2
 
@@ -103,7 +107,7 @@ def __get_userid_from_key(key):
 def __product_is_in_DB(barcode):
     # Check if the product is in the DB
     # Return bool
-    sql = "SELECT * FROM ProductInformation WHERE ProductID = '%s';" % (key)
+    sql = "SELECT * FROM ProductInformation WHERE ProductID = '%s';" % (barcode)
     db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
     log("Connected to DB", 3)
     cursor = db.cursor()
@@ -112,7 +116,6 @@ def __product_is_in_DB(barcode):
     data = cursor.fetchone()
     db.close()
     log("DB closed", 3)
-
     if data:
         return True
     else:
@@ -141,6 +144,7 @@ def __group_in_db(name):
 def __add_group_to_db(name):
     # Adds group to DB
     # Return bool
+    log("Add group to DB request", 2)
     sql = "INSERT INTO `KitchenWizard`.`Grouping` (`GroupName`, `DateAdded`) VALUES ('%s', '%s');" % (str(name), str(datetime.now()))
     db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
     log("Connected to DB", 3)
@@ -184,25 +188,27 @@ def __get_product_details_from_api(barcode):
     url = "http://api.foodessentials.com/labelarray?u=%s&sid=9fe4492b-492e-4336-86b8-7d278e02aa51&n=2&s=0&f=json&api_key=mvdrrzwt9327ttazxse6f95b" % (str(barcode))
     response = urlopen(url)
     j_obj = json.load(response)
-    item = {}
+    item = []
     for i in j_obj['productsArray']:
-        item.add(str(barcode))
-        item.add(str(i['product_name']))
-        item.add(str(i['product_description']))
-        item.add(str(i['manufacturer']))
-        item.add(str(i['product_size']))
-        item.add(str(__get_group_id(str(i['shelf']))))
+        item.append(str(barcode))
+        item.append(str(i['product_name']))
+        item.append(str(i['product_description']))
+        item.append(str(i['manufacturer']))
+        item.append(str(i['product_size']))
+        item.append(str(__get_group_id(str(i['shelf']))))
     return item
 
 def __add_product_to_DB(item):
     # Add product details from API to Database
     # Return bool
     #{0: Barcode, 1: Name, 2:details, 3:Maker, 4:size, 5:group}
-    sql = "INSERT INTO ProductInformation (ProductID, ProductName, ProductDiscription, Manufacturer, Quantity, GroupID) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (str(item[0]), str(item[1]), str(item[2]), str(item[3]), str(item[4]), str(item[5]))
+    log("Add product request", 2)
+    sql = "INSERT INTO `KitchenWizard`.`ProductInformation` (`ProductID`, `ProductName`, `ProductDiscription`, `Manufacturer`, `Quantity`, `GroupID`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (str(item[0]), str(item[1]), str(item[2]), str(item[3]), str(item[4]), str(item[5]))
     db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
     log("Connected to DB", 3)
     cursor = db.cursor()
     try:
+        log("Inside try", 3)
         cursor.execute(sql)
         log("SQL excuted correctly", 2)
         db.commit()
@@ -225,6 +231,7 @@ def __add_item_to_inventory(barcode, userid):
             return False
 
     # Add item to inventory
+    log("Add to inventory request", 2)
     sql = "INSERT INTO `KitchenWizard`.`Inventory` (`UserID`, `ProductID`, `DateAdded`) VALUES ('%s', '%s', '%s');" % (str(userid), str(barcode), str(datetime.now()))
     db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
     log("Connected to DB", 3)
@@ -246,7 +253,7 @@ def __add_item_to_inventory(barcode, userid):
 def __get_inventory_id(barcode, userid):
     # Get id for latest item added with particular barcode by user
     # Return str
-    sql = "SELECT InventoryID FROM Inventory WHERE ProductID = 's' AND UserID = '%s';" % (str(barcode), str(userid))
+    sql = "SELECT InventoryID FROM Inventory WHERE ProductID = '%s' AND UserID = '%s';" % (str(barcode), str(userid))
     db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
     log("Connected to DB", 3)
     cursor = db.cursor()
@@ -255,7 +262,7 @@ def __get_inventory_id(barcode, userid):
     data = cursor.fetchall()
     db.close()
     log("DB closed", 3)
-    return str(data[(len(data) - 1)])
+    return str(data[(len(data)-1)])
 
 def add_new_item(barcode, session):
     # Add new item to inventory of user
@@ -266,18 +273,9 @@ def add_new_item(barcode, session):
         log("Add item failed", 1)
         return "ADD_FAILED"
     else:
-        item =  __get_product_details(barcode)
-        if item:
-            if __add_item_to_inventory(item):
-                log("New item added", 1)
-                return __get_inventory_id(barcode, userid)
-            else:
-                log("Add item failed", 1)
-                return "ADD_FAILED"
+        if __add_item_to_inventory(barcode, userid):
+            log("New item added", 1)
+            return __get_inventory_id(barcode, userid)
         else:
-            ("Add item failed", 1)
+            log("Add item failed", 1)
             return "ADD_FAILED"
-
-
-# Testing code
-#__get_product_details_from_api("024100440771")
