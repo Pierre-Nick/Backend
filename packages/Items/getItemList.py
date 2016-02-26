ad7d342d7b860292a2750902c10b2039f487a81a
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-02-26 18:10:29 -0500

    Updating Get Item Script
    Updating get items script, testing is required to complete this script.

--- packages/Items/getItemList.py
+++ packages/Items/getItemList.py
@@ -22,4 +22,79 @@ def log(message, lev):
     if debug_on:
         if lev <= log_level:
             ti = str(datetime.now())
-            print("[%s]addItem --> %s" % (ti, message))
+            print("[%s]getItemList --> %s" % (ti, message))
+
+
+def __get_product_information(ProductID):
+    # Get the product information
+    # return list
+    log("Get product information", 2)
+    sql = "SELECT * FROM ProductInformation WHERE ProductID = '%s';" % (ProductID)
+    db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
+    log("Connected to DB", 3)
+    cursor = db.cursor()
+    cursor.execute(sql)
+    log("SQL excuted correctly", 3)
+    data = cursor.fetchone()
+    db.close()
+    log("DB closed", 3)
+    return data
+
+
+def __get_group_image(group_id):
+    # Get the image of a product for a group
+    # return str
+    log("Get url for group image", 2)
+    sql = "SELECT GroupImage FROM Grouping WHERE GroupID = '%s';" % (group_id)
+    db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
+    log("Connected to DB", 3)
+    cursor = db.cursor()
+    cursor.execute(sql)
+    log("SQL excuted correctly", 3)
+    data = cursor.fetchone()
+    db.close()
+    log("DB closed", 3)
+    return str(data[0])
+
+
+def __get_items_for_user(userid):
+    # Get items for a userid
+    # Return list
+    log("Get items for user from DB", 2)
+    sql = "SELECT ProductID FROM Inventory WHERE UserID = '%s';" % (userid)
+    db = MySQLdb.connect("localhost","kitchenWizard","","KitchenWizard")
+    log("Connected to DB", 3)
+    cursor = db.cursor()
+    cursor.execute(sql)
+    log("SQL excuted correctly", 3)
+    data = cursor.fetchall()
+    db.close()
+    log("DB closed", 3)
+    return data
+
+
+def __create_response_list(items):
+    # Create and return the list of products
+    # Return list
+    temp_list = []
+    final_list = []
+    log("Create list of products, for UI", 2)
+
+    for i in items:
+        temp_list.append(__get_product_information(str(i))
+
+    for i in temp_list:
+        k={i[0], i[1], i[2], i[3], i[4], __get_group_image(str(i[5])}
+        print k
+        final_list.append(k)
+
+    print final_list
+
+
+def get_item_list(session_key):
+    # Get items for a userid
+    # Return list
+    log("Get items for a user", 1)
+    userid = __get_userid_from_key(session_key)
+    user_list = __get_items_for_user(userid)
+    return __create_response_list(user_list)
