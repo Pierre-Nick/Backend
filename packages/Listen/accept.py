55199088826941850810f4775146a8252eca2cf3
rmullins93 <rmullins93@gmail.com>
2016-03-04 15:23:53 -0500

    fixed stuff

--- packages/Listen/accept.py
+++ packages/Listen/accept.py
@@ -5,6 +5,7 @@ from packages.Job import manager
 def listen():
 	try:
 		serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		serversocket.bind(('', 8080))
 		serversocket.listen(5)
 		kwlog.log("Server is listening")
