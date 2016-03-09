71854b7b5d8c5a1e2c43ae90afa807c4fed4be8b
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-03-08 22:57:36 -0500

    updates to file structure

--- test.py
+++ test.py
@@ -3,5 +3,3 @@ while True:
     url = "http://127.0.0.1:8080?command=additem&barcode=%s&sessionkey=2178071641&" % (str(input("Barcode: ")))
     response = urlopen(url).read().encode("utf-8")
     print(response)
-
-    
