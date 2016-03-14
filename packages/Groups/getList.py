d3a02612d99bf066beeb6f8b9d088cb87ca59c7a
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-03-14 00:34:44 -0400

    Adding get list of generic items

---
+++ packages/Groups/getList.py
@@ -0,0 +1,19 @@
+###################################################################
+#####   `   Add item for KitchenWizard                        #####
+###################################################################
+##### Version: 0.3                                            #####
+##### Author:  Marcus R                                       #####
+##### Tested:  --                                             #####
+#####                                                         #####
+##### Purpose: The primary purpose of this mod is to get a    #####
+#####          list of generic items                          #####
+###################################################################
+
+from packages.Log import kwlog
+from packages.Database import MySQL
+
+
+def get_list_of_generic_items():
+    # Get list of generic Items
+    # Return: str
+    return str()
