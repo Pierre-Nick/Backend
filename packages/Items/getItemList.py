b7120d12315052e6f89ae2da882aa06e4437d4ea
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-03-14 23:25:30 -0400

    Now, I hope!

--- packages/Items/getItemList.py
+++ packages/Items/getItemList.py
@@ -43,7 +43,10 @@ def __create_response_list(items):
 
     for i in items:
         temp = __get_product_information(str(i[1]))
-        k = [i[0], temp[0], temp[1], temp[2], temp[3], temp[4], str(__get_group_name(str(temp[5]))[0]), temp[6], temp[7], temp[8]]
+        if __get_group_name(str(temp[5])):
+            k = [i[0], temp[0], temp[1], temp[2], temp[3], temp[4], str(__get_group_name(str(temp[5]))[0]), temp[6], temp[7], temp[8]]
+        else:
+            k = [i[0], temp[0], temp[1], temp[2], temp[3], temp[4], "NONE", temp[6], temp[7], temp[8]]
         final.append(k)
 
     return final
