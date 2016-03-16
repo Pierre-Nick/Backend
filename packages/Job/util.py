c5f6a053a08f27a17fb49740b1537fc20b786b23
rmullins93 <rmullins93@gmail.com>
2016-03-16 19:41:17 -0400

    semicolons

--- packages/Job/util.py
+++ packages/Job/util.py
@@ -15,7 +15,7 @@ def replace_commas_with_semicolons(data):
 	
 	in_string = False
 	for i in range(len(data)):
-		if data[i] == "\"":
+		if data[i] == "\'":
 			in_string =  not in_string
 		elif data[i] == "," and  not in_string:
 			data = data[:i] + ';' + data[i+1:]
