48836e0fc14cded849d6bfaf39cb3b31425d3029
rmullins93 <rmullins93@gmail.com>
2016-03-20 17:05:05 -0400

    check for apostrophees

--- packages/Job/util.py
+++ packages/Job/util.py
@@ -15,7 +15,7 @@ def replace_commas_with_semicolons(data):
 
 	in_string = False
 	for i in range(len(data)):
-		if data[i] == "\'" or data[i] == "\"":
+		if (data[i] == "\'" or data[i] == "\"") and not re.search(r'[a-zA-Z\d]', data[i+1]):
 			in_string =  not in_string
 		elif data[i] == "," and  not in_string:
 			data = data[:i] + ';' + data[i+1:]
