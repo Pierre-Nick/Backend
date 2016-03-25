b1d56224a1286863e1446a25d2941d4d3f6db93f
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2016-03-25 15:05:24 -0400

    Mistake 2

--- packages/Database/MySQL.py
+++ packages/Database/MySQL.py
@@ -577,7 +577,7 @@ def insert_item_to_list(sid, gid, measurment, userid):
         return False
 
 def get_list_of_shopping_items(sid):
-    sql = "SELECT * FROM Shopping_Item WHERE ShoppingList = %s;"\
+    sql = "SELECT * FROM Shopping_Item WHERE ShoppingList = %s;"
     cursor.execute(sql, sid)
     return cursor.fetchall()
 
